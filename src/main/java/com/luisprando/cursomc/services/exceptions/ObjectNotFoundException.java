24f6c0e7938fc699219f97ef4e4e2fbd5241cbf5
Luis Prando <luisenprando@outlook.com>
2018-10-01 13:16:10 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/luisprando/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.luisprando.cursomc.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final Long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+	
+}
